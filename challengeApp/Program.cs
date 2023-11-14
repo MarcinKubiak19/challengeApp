c9ca6a7979aeea04a29e29d2d816920c267d478c
Marcin Kubiak <marcinkubiak19@gmail.com>
2023-11-14 22:49:31 +0100

    drugi commit

--- challengeApp/Program.cs
+++ challengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var age = 33;
+string name = "EWA";
+bool isFemale = true;
+
+if (age < 30 && isFemale == true)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (age == 33 && isFemale == true && name == "EWA")
+{
+    Console.WriteLine("EWA lat 33");
+}
+else if (age < 18 && isFemale == false)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{ 
+    Console.WriteLine("Kryteria nie zostały spełnione"); 
+}
