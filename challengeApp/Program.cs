5fb6c70b9bf22732c0c1925a97689add503be245
Marcin Kubiak <marcinkubiak19@gmail.com>
2023-11-15 23:14:22 +0100

    trzeci commit

--- challengeApp/Program.cs
+++ challengeApp/Program.cs
@@ -1,20 +1,36 @@
-﻿var age = 33;
-string name = "EWA";
-bool isFemale = true;
+﻿int number = 4566;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
-if (age < 30 && isFemale == true)
+foreach (char letter in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (letter == '0') { counter0++; }
+    if (letter == '1') { counter1++; }
+    if (letter == '2') { counter2++; }
+    if (letter == '3') { counter3++; }
+    if (letter == '4') { counter4++; }
+    if (letter == '5') { counter5++; }
+    if (letter == '6') { counter6++; }
+    if (letter == '7') { counter7++; }
+    if (letter == '8') { counter8++; }
+    if (letter == '9') { counter9++; }
 }
-else if (age == 33 && isFemale == true && name == "EWA")
-{
-    Console.WriteLine("EWA lat 33");
-}
-else if (age < 18 && isFemale == false)
-{
-    Console.WriteLine("Niepełnoletni Mężczyzna");
-}
-else
-{ 
-    Console.WriteLine("Kryteria nie zostały spełnione"); 
-}
+Console.WriteLine("1 => " + counter1);
+Console.WriteLine("2 => " + counter2);
+Console.WriteLine("3 => " + counter3);
+Console.WriteLine("4 => " + counter4);
+Console.WriteLine("5 => " + counter5);
+Console.WriteLine("6 => " + counter6);
+Console.WriteLine("7 => " + counter7);
+Console.WriteLine("8 => " + counter8);
+Console.WriteLine("9 => " + counter9);
