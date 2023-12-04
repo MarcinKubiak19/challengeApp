c6de5a5cd86bc238dc01aac065a0247f831b7a26
Marcin Kubiak <marcinkubiak19@gmail.com>
2023-12-05 00:09:17 +0100

    Commit Unit7 i 8

---
+++ challengeApp.Tests/EmployeeTest.cs
@@ -0,0 +1,20 @@
+﻿using challengeApp;
+
+namespace ChallengeApp.Tests
+{
+    public class Tests
+    {
+
+        [Test]
+        public void CheckingAddScoreOperation()
+        {
+            var employee = new Employee("Adam", "Nowak", 34);
+            employee.AddScore(5);
+            employee.AddScore(6);
+            employee.AddScore(-12);
+            var result = employee.Result;
+
+            Assert.AreEqual(-1, result);
+        }
+    }
+}
